d9cba84ff5a265492e5ace5b3d66d77128cc557f
Peter Sallai <peter.sallai@badaccess.eu>
2025-10-20 21:17:37 +0200

    Added api_key variable

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use spin_sdk::{
     http::{IntoResponse, Response},
     http_component,
+    variables
 };
 
 
@@ -8,12 +9,15 @@ use spin_sdk::{
 fn cloud_start(req: http::Request<()>) -> anyhow::Result<impl IntoResponse> {
     println!("{:?}", req.headers());
 
-    let body = "
+    let api_key = variables::get("api_key")?;
+
+    let body = format!("
     <html>
     <body>
         <h1>Welcome to Moonblokz Telemetry Hub</h1>
+        <p>Your API key is: {}</p>
     </body>
-    </html>";
+    </html>", api_key);
 
     Ok(Response::builder()
         .status(200)
